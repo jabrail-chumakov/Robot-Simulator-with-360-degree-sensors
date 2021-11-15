ead39c42079aa93c1b817ef5656d97cc736b4bb3
Jabrail Chumakov <67557966+jabrail-chumakov@users.noreply.github.com>
2021-11-15 22:22:54 +0500

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -9,10 +9,14 @@ o_th = 2.0
 d_th = 0.4
 """ float: Threshold for the control of the linear distance """
 
-g_th = 0.6
-""" float: Threshold for the control of the distance to the nearest golden box """
+g_th = 0.7
+""" float: Threshold for the control of the distance to the nearest golden token """
 
+golden_angle_max = 105.0
+""" float: Threshold for the maximum golden angle for the special case """
 
+golden_angle_min = 75.0
+""" float: Threshold for the minimum goolden angle for the special case """	
 
 R = Robot()
 """ instance of the class Robot """
@@ -84,36 +88,97 @@ def find_golden_token():
     	return dist_golden, rot_golden
 
 def place_silver_token():
-	print("Silved box was grabbed")
+	"""
+	Function to place silver token behind when it's successfully grabbed
+	"""
+	print("Silved token was grabbed")
 	turn(40, 1.5)
 	drive(20, 1)
 	R.release()
-	print("Silver box was released")
+	print("Silver token was released")
 	drive(-20, 1)
 	turn(-40, 1.5)
+	print("Robot returned to the initial position")
+
+def special_avoiding_case():
+	"""
+	Function to check whether there is an obtacle in approximately 90 degree radius (from 75 to 105 degrees)
+	If distance to the golden token is closer from the left, then robot turns right
+	While in case if distance to the goldex token is closer from the right, then robot turns left
+	
+	Return:
+		0 (False): Robot turs right
+		1 (True): Robot turns left
+	"""
+	dist = 100
+	# Robot scans for the golden token from the left side
+    	for token in R.see():
+    		if golden_angle_min < token.rot_y < golden_angle_max and token.dist < dist:
+    			dist = token.dist
+   	 		dist_golden = dist
+   	 		rot_y = token.rot_y
+        		rot_golden = rot_y
+   	left_side = dist_golden
+
+	# Robot scans for the golden token from the right side
+   	for token in R.see():
+   	 	if -golden_angle_max < token.rot_y < -golden_angle_min and token.dist < dist:
+   	 		dist = token.dist
+   	 		dist_golden = dist
+   	 		rot_y = token.rot_y
+        		rot_golden = rot_y
+   	right_side = dist_golden
+
+   	return left_side > right_side
+   	
+""" 		
+Working principle
+
+(1) If you are far away from the nearest golden token or angle to the nearest golden token is more than 90 degrees
+then robot moves straight
+
+(2) If the robot detects a silver token on the distance closer than 1.00, then robot aligns and moves towards it
+
+(3) If the robot on the distance of 0.4 from the silver token, then grabs it, puts it behind and returns to the initial position
+
+(4) If the robot on the distance of 0.7 from the golden token and absolute golden angle is less than 90 degress, then:
+	(a) If angle is more or equal to 25: Robot turns left
+	(b) If angle is less or equal to -25: Robot turns right
+	(c) Otherwise, a special avoiding case is executed which checks where to turn depending on the closest golden token distance
+"""
 
 while 1:
 	dist_silver, rot_silver = find_silver_token()
 	dist_golden, rot_golden = find_golden_token()	
+	
+	# When silver token is near and ready to be grabbed (2) and (3)
 	if dist_silver < d_th:
-		print("Token detected!")
+		print("Silver token detected!")
 		if R.grab():
 			place_silver_token()
-			
+	
+	# Robot moves straight (1)
 	if  dist_golden > g_th or abs(rot_golden) > 90.0:	
 		if 100.0 > rot_silver > o_th and dist_silver < 1.0:
 			turn(10, 0.01)
 		elif -o_th > rot_silver > -100.0 and dist_silver < 1.0:
 			turn(-10, 0.01)		
 		else:
-			#print("Moving in the search of silver box!")
-			print("Golden angle: ", rot_golden)
+			print("Moving in the search of silver box!")
 			drive(30, 0.1)	
 			
+	# When golden token is near (4)
 	elif dist_golden < g_th and abs(rot_golden) < 90.0:
-		if rot_golden < 0.0:
-			print("Left")
-			turn(20, 0.5)
-		elif rot_golden > 0.0:
-			print("Right")
-			turn(-20, 0.5)
+		if rot_golden <= -25.0:
+			print("Turn right")
+			turn(10, 0.5)
+		elif rot_golden >= 25.0:
+			print("Turn left")
+			turn(-10, 0.5)
+		else:
+			if special_avoiding_case(): 
+				print("Special case: Right!")
+				turn(10, 0.5)
+			else:
+				print("Special case: Left!")
+				turn(-10, 0.5)
